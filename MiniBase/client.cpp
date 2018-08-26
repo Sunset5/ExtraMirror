b41624ef3e874d0867ac26e6625dc90a795279dc
shelru <2@shelru.ru>
2018-08-26 16:52:32 +0400

    Console color chat fixes

--- MiniBase/client.cpp
+++ MiniBase/client.cpp
@@ -126,7 +126,7 @@ typedef enum cmd_source_s
 
 void InitHack(){
 	if (g_Engine.Con_IsVisible() == 0)g_Engine.pfnClientCmd("toggleconsole");
-	ConsolePrintColor(0, 255, 11, "-- Extra Mirror v3.01\n");
+	ConsolePrintColor(0, 255, 11, "-- Extra Mirror v3.03\n");
 	ConsolePrintColor(255, 255, 255, "-- Use 'credits' for more information\n");
 	ConsolePrintColor(255, 255, 255, "-- Thank's to Realwar for title\n");    
 	ConsolePrintColor(255, 255, 255, "-- Thank's to FightMagister for functions\n");
